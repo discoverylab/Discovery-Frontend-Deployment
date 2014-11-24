2461f6716c3412740c311ee1b4693f390e01a04f
MohanRaj Balumuri <mbalumur@asu.edu>
2014-11-24 14:31:50 -0700

    login page

--- Frontend/server/app.js
+++ Frontend/server/app.js
@@ -8,10 +8,23 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 //process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+/*
+var https = require('https');
+var fs         = require("fs");
+var key_file   = "certs/file.pem";
+var cert_file  = "certs/file.crt";
+
+var httpsconfig     = {
+  key: fs.readFileSync(key_file),
+  cert: fs.readFileSync(cert_file)
+};
+*/
+
 var express = require('express');
 var config = require('./config/environment');
 // Setup server
 var app = express();
+//https.createServer(httpsconfig, app).listen(3443);
 var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
